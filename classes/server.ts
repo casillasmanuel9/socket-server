cdba5df5af7bb60d13f4ba39a5a47f2cc2ee5d11
Juan Manuel Galindo Casillas <casillasmanuel9@gmail.com>
2020-05-22 18:00:05 -0500

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,39 @@
-import { SERVER_PORT } from './../global/enviroment';
-import express from 'express';
+import { SERVER_PORT } from "./../global/enviroment";
+import express from "express";
+import socketIO from "socket.io";
+import http from "http";
+import * as sokect from '../sockets/sockets';
 
 export default class Server {
-    public app: express.Application;
-    public port: number;
+  private static _instance: Server;
 
-    constructor() {
-        this.app = express();
-        this.port = SERVER_PORT
-    }
+  public app: express.Application;
+  public port: number;
+  public io: socketIO.Server;
+  private httpServer: http.Server;
 
-    start(callback: VoidFunction) {
-        this.app.listen(this.port, callback);
-    }
-} 
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log("Escuchando conexiones");
+    this.io.on("connection", (cliente) => {
+      console.log("Nuevo Cliente Conectado");
+      sokect.mensaje(cliente, this.io);
+      sokect.desconectar(cliente);
+    });
+  }
+
+  start(callback: VoidFunction) {
+    this.httpServer.listen(this.port, callback);
+  }
+}
